c6bea0cf0e1d77a8cd03624b358f431a285c83ec
Carlos Astorga <carlos.astorgax@gmail.com>
2020-12-29 19:38:46 +0100

    img alt

--- proyecto_final/peso_sitio_web.py
+++ proyecto_final/peso_sitio_web.py
@@ -14,7 +14,7 @@ print("Resultado: ", resultado.geturl())
 
 #---------- Peso de la página ----------#
 url = "https://python.org"
-print ("URL: ", url)
+print("URL: ", url)
 site = request.urlopen(url)
 meta = site.info()
 print("Content-Length: ", site.headers['content-length'])
@@ -22,9 +22,10 @@ print("Content-Length: ", site.headers['content-length'])
 #---------- Verificar description ----------#
 site = request.urlopen('https://python.org')
 soup = BeautifulSoup(site, "html.parser")
-description = soup.find('meta', attrs={'name':'description'})
-print('El tamaño de la description de python.org es: ', len(description.get('content')))
-if (len(description.get('content')))<154:
+description = soup.find('meta', attrs={'name': 'description'})
+print('El tamaño de la description de python.org es: ',
+      len(description.get('content')))
+if (len(description.get('content'))) < 154:
     print("La descripción es menor a 154")
 
 #---------- Verificar etiqueta titulo ----------#
@@ -36,27 +37,18 @@ print("y dice: ", soup.html.head.title.string)
 #---------- Obtener las Palabras clave del sitio ----------#
 site = request.urlopen('https://python.org')
 soup = BeautifulSoup(site, "html.parser")
-keywords = soup.find('meta', attrs={'name':'keywords'})
+keywords = soup.find('meta', attrs={'name': 'keywords'})
 print('Keywords: ', keywords.get('content'))
 words = keywords.get('content').split()
 print(words)
 for word in words:
-    print(word,len(soup.findAll(text=re.compile(word))))
+    print(word, len(soup.findAll(text=re.compile(word))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#---------- Obtener el texto alternativo de las imágenes del sitio ----------#
+site = request.urlopen('https://python.org')
+soup = BeautifulSoup(site, "html.parser")
+count = 1
+for image in soup.findAll('img'):
+    print('imagen #1: ', count, ':', image["src"])
+    print("Alt. de la imagen: ", count, ':', image.get('alt', 'None'))
+    count += 1
