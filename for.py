b0ad33886475fc658b1f2b323ef3b176acd4588f
Carlos Astorga <carlos.astorgax@gmail.com>
2021-01-05 22:12:23 +0100

    otro ej. for

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-nombre = "Carlos"
+""" nombre = "Carlos"
 edades = '120'
 x = 1
 
@@ -9,4 +9,15 @@ for edad in edades:
     print(edad)
 
 for x in range (1,50):
-    print(x)
+    print(x) """
+
+#Ejemplo2
+frutas = ["manzana","platano","naranja","uva","durazno","piña"]
+for e in frutas:
+    print(frutas)
+
+for e in frutas:
+    print(e)
+
+for e in frutas:
+    print(frutas[3])
