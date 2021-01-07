96a06088eeb4fef393bad85357326b2f8925176d
Carlos Astorga <carlos.astorgax@gmail.com>
2021-01-07 14:02:21 +0100

    class ej

--- class.py
+++ class.py
@@ -1,4 +1,5 @@
-class Persona:
+#Ejemplo 1
+""" class Persona:
     nombre="Carlos"
     apellido="Astorga"
     edad=0
@@ -14,4 +15,23 @@ class Persona:
 saludo =  Persona()
 saludo.saludar()
 print("-----------------")
-saludo.despedir()
+saludo.despedir() """
+
+#Ejemplo 2
+class Automovil():
+    modelo = "sedan"
+    puertas = 4
+    combustible="gasolina"
+    cilindradada=2
+    encendido = False
+
+    def encender(self):
+        self.encendido = True
+
+audi_a4 = Automovil() #creo la instancia audi_a4
+
+audi_a4.encender() # llamamos a el método, el cual cambiará el encendido a True
+
+print(audi_a4.encendido)
+
+print("El vehículo es un", audi_a4.modelo, "de", audi_a4.puertas, "puertas")
