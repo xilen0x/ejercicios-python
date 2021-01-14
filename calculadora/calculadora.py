a5fc7e538803330e91f011c9b4fcd9b0916e16e4
Carlos Astorga <carlos.astorgax@gmail.com>
2021-01-14 16:03:52 +0100

    calc first commit

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -7,35 +7,43 @@ myFrame.pack()
 
 operacion = ""
 resultado = 0
+memoria = 0
 #------------------------------------PANTALLA--------------------------------#
 numeros = StringVar()
 pantalla=Entry(myFrame, textvariable=numeros)
 pantalla.grid(row=1, column=1, padx=10, pady=10, columnspan=4) #con columnspan le indicamos que esta fila debe ocupar las 4 columnas.
 pantalla.config(background="black", fg="#03f943", justify="right", font=(28))
 
+root.title("Simple Calculator")
+root.geometry("250x200")
 #------------------------------------Funciones Botones --------------------------------
 def clickBoton(num):
     global operacion
     if operacion!="":
-        numeros.set(num)
+        memoria = numeros.set(num)
         operacion=""
     else:
-        numeros.set(numeros.get() + num) #obtengo lo q hay en numeros, le concateno el parámetro y lo muestro en la pantalla
+        memoria = numeros.set(numeros.get() + num) #obtengo lo q hay en numeros, le concateno el parámetro y lo muestro en la pantalla
         
 def clickBotonC():
     numeros.set("0")
     
+#----------------------------función suma
 def suma(num):
     global operacion, resultado
-    resultado+=int(num)
-    operacion = "suma"
-    numeros.set(resultado)        
+    resultado = int(num) + int(memoria)
+    numeros.set(resultado)
+    #resultado = 0
+
+#----------------------------función resta
+def resta(num):
+    pass       
 
 #función miResultado para el boton del =
 def miResultado():
     global resultado
     numeros.set(resultado+int(pantalla.get()))    
-    resultado=0    
+    resultado= 0    
 
 
 #------------------------------------FILA1 BOTONES 7,8,9,/ --------------------------------
@@ -64,7 +72,7 @@ botonMult.grid(row=3, column=4)
 boton1 = Button(myFrame, text="1", width=3, command=lambda:clickBoton("1"))
 boton2 = Button(myFrame, text="2", width=3, command=lambda:clickBoton("2"))
 boton3 = Button(myFrame, text="3", width=3, command=lambda:clickBoton("3"))
-botonRest = Button(myFrame, text="-", width=3)
+botonRest = Button(myFrame, text="-", width=3, command=lambda:resta(numeros.get()))
 
 boton1.grid(row=4, column=1)
 boton2.grid(row=4, column=2)
