dd3ae00145ca2540ccca959512416fdcd27be27e
Carlos Astorga <carlos.astorgax@gmail.com>
2021-01-19 15:32:49 +0100

    ajustes

--- CRUD PYTHON/app.py
+++ CRUD PYTHON/app.py
@@ -1,39 +1,62 @@
 from tkinter import *
 from tkinter import messagebox
+from conexion_db import *
+import sqlite3
 
 root = Tk()
 miMenu = Menu(root)
 root.config(menu = miMenu)
 miFrame = Frame(root)
 miFrame.pack()
+
 root.geometry('500x400')
 root.resizable(0,0)
 root.title("My CRUD in Python")
-#-----------------------------Funciones:-------------------------------------
+#-----------------------------Funciones Menú:-------------------------------------
 def ventanaEmergenteInfo():
     messagebox.showinfo("titulo ventana","Mensaje de Información")
-def ventanaEmergenteWarn():
-    messagebox.showwarning("titulo ventana","Mensaje de advertencia")
+def ErrorConexion():
+    messagebox.showwarning("Error de conexión", "Tabla USUARIOS ya existe")
 def salir():
-    valor = messagebox.askquestion("Salir", "Estas segurod e salir?")
+    valor = messagebox.askquestion("Salir", "Estas seguro de salir?")
     if valor == "yes":
         root.destroy()
 
+#----------------------------Función conexión-------------------------------------
+def conexion_dataBase():
+    try:        
+        miConexion = sqlite3.connect("MiBase")
+        miCursor = miConexion.cursor()
 
-#-----------------------------SUBMENUS:-------------------------------------
+        miCursor.execute('''
+        CREATE TABLE USUARIOS (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE VARCHAR(50) UNIQUE,
+        PASSWORD VARCHAR(50),
+        DIRECCION VARCHAR(100)
+        )
+        ''')
+
+        miConexion.commit()
+        miConexion.close()
+
+    except:
+        ErrorConexion()
+
+
+#-----------------------------CREACIÓN DE MENUS(SUBMENUS):-------------------------------------
 conectarDB = Menu(miMenu, tearoff=0) #tearoff=0 para eliminar submenu vacío o rayitas--
-conectarDB.add_command(label="Salir", command=salir)
-
+conectarDB.add_command(label="Conectar", command=conexion_dataBase)
 
 limpiarMenu = Menu(miMenu, tearoff=0)
-
+limpiarMenu.add_command(label="Limpiar campos")
 
 ayudaMenu = Menu(miMenu, tearoff=0)
 ayudaMenu.add_command(label="Ventana Emergente Info", command=ventanaEmergenteInfo)
-ayudaMenu.add_command(label="Ventana Emergente Warning", command=ventanaEmergenteWarn)
+ayudaMenu.add_command(label="Salir", command=salir)
 
-#-----------------------------MENÚ PRINCIPAL:-------------------------------------
-miMenu.add_cascade(label="Conectar DB", menu=conectarDB)
+#-----------------------------ANEXAR LOS MENÚS CREADOS A LA BARRA DE MENU: miMenu:-------------------------------------
+miMenu.add_cascade(label="BBDD", menu=conectarDB)
 miMenu.add_cascade(label="Limpiar Formulario", menu=limpiarMenu)
 miMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
 
@@ -42,15 +65,25 @@ entryID = Entry(miFrame)
 entryID.grid(row=0, column=1, padx=20, pady=10)
 entryID.config(fg="red") 
 
-entryNombre = Entry(miFrame)
+miNombre=StringVar()
+entryNombre = Entry(miFrame, textvariable=miNombre)
 entryNombre.grid(row=1, column=1, padx=10, pady=10)
 
-entryPass = Entry(miFrame)
+miPass=StringVar()
+entryPass = Entry(miFrame, textvariable=miPass)
 entryPass.grid(row=2, column=1, padx=10, pady=10)
 entryPass.config(show="*") # para reemplazar el password por asteriscos
 
-entryDireccion = Entry(miFrame)
+miDirec=StringVar()
+entryDireccion = Entry(miFrame, textvariable=miDirec)
 entryDireccion.grid(row=3, column=1, padx=10, pady=10)
+
+entryComentario=Text(miFrame, width=23, height=5)
+entryComentario.grid(row=4, column=1, padx=10, pady=10)
+scrollVert = Scrollbar(miFrame, command=entryComentario.yview)
+scrollVert.grid(row=4, column=2, sticky="nsew")
+entryComentario.config(yscrollcommand=scrollVert.set)
+
 #----------------------Labels-------------------------------------------:
 labelID= Label(miFrame, text="ID: ")
 labelID.grid(row=0, column=0, sticky="e")
@@ -64,33 +97,40 @@ labelPass.grid(row=2, column=0, sticky="e")
 labelDireccion = Label(miFrame, text="Dirección: ")
 labelDireccion.grid(row=3, column=0, sticky="e") 
 
-#----------------------Buttons CRUD-------------------------------------------:
-#------CREAR--------
+labelComentario = Label(miFrame, text="Comentario: ")
+labelComentario.grid(row=4, column=0, sticky="e") 
+
+#------------new frame for buttons-----#
+buttonFrame = Frame(root)
+buttonFrame.pack()
+
+# ---***********************--Button function-CREAR---***********************---
 def createData():
-    ventanaEmergenteInfo()
+    #miCursor.executemany('INSERT INTO USUARIOS VALUES(NULL,?,?,?)', miNombre, miPass, miDirec ) #AQUI VOY!!!!!!!!!!******************
+    pass
 
-botonCrear = Button(root, text="Guardar", command=createData) #codigoBoton es una funcion
-botonCrear.pack()
+botonCrear = Button(buttonFrame, text="Guardar", command=createData)
+botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
-#------LEER--------
+# ---***********************--Button function-LEER---***********************---
 def readData():
     ventanaEmergenteInfo()
 
-botonLeer = Button(root, text="Listar", command=readData)
-botonLeer.pack()
+botonLeer = Button(buttonFrame, text="Listar", command=readData)
+botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
 
-#------ACTUALIZAR--------
+# ---***********************--Button function-ACTUALIZAR---***********************---
 def updateData():
     ventanaEmergenteInfo()
 
-botonActualizar = Button(root, text="Actualizar", command=updateData)
-botonActualizar.pack()
+botonActualizar = Button(buttonFrame, text="Actualizar", command=updateData)
+botonActualizar.grid(row=1, column=2, sticky="e", padx=10, pady=10)
 
-#------BORRAR--------
+# ---***********************--Button function-BORRAR---***********************---
 def deleteData():
     ventanaEmergenteInfo()
 
-botonBorrar = Button(root, text="Borrar", command=deleteData) #codigoBoton es una funcion
-botonBorrar.pack()
+botonBorrar = Button(buttonFrame, text="Borrar", command=deleteData) 
+botonBorrar.grid(row=1, column=3, sticky="e", padx=10, pady=10)
 
 root.mainloop()
